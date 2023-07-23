4883b9ee42f3806e9b5f182a6f25076b03f0d7c9
MohimSingla <Mohim.singla@skeps.com>
2023-07-23 13:50:56 +0530

    Api end points creation

--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { User } = require('../models/model.js')
+const { User, Book } = require('../models/model.js')
 
 const router = new express.Router();
 
@@ -7,29 +7,59 @@ router.post('/auth/login', async (req, res) => {
     try {
         const user = await User.findByCredentials(req.body.userName, req.body.password);
         res.send({ user });
-    } catch (e) {
+    } catch (error) {
         res.status(400).send("Invalid Request!");
     }
 })
 
-router.post('/books' , (req, res) => {
-    res.send("post req books");
+router.post('/books' , async (req, res) => {
+    try{
+        const bookData = await new Book(req.body);
+        await bookData.save();
+        res.send("Book data saved successfully.");
+    }
+    catch(error){
+        res.status(500).send(error.message);
+    }
 })
 
-router.get('/books', (req, res) => {
-    res.send("hey good progress");
+router.get('/books', async (req, res) => {
+    try{
+        const booksData = await Book.find();
+        res.send(booksData);
+    }
+    catch(error){
+        res.status(404).send("Invalid Request");
+    }
 })
 
-router.get('/books/:id', (req, res) => {
-    res.send("Individual book")
+router.get('/books/:id', async (req, res) => {
+    try{
+    const _id = req.params.id;
+    const bookData = await Book.findById(_id);
+    if(!bookData){
+        throw new Error("Book with the requested ID not found.")
+    }
+    res.send(bookData);
+    }
+    catch(error){  
+        res.status(404).send(error.message);
+    }    
 })
 
 router.put('/books/:id', (req, res) => {
     res.send("Update Function")
 })
 
-router.delete('/books/:id', (req, res) => {
-    res.send("Delete operation")
+router.delete('/books/:id', async (req, res) => {
+    try{
+        const _id = req.params.id;
+        await Book.deleteOne({_id});
+        res.send("Requested Book data deleted successfully.");
+    }
+    catch(error){
+        res.send("Unable to delete the requested book. Kindly verify the entered details or Please try again later if the error still persists!");
+    }
 })
 
 module.exports = {router};
